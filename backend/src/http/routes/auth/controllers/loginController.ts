e25f5d575615dc91216c1d28343bdfdcf11eda69
Yoni Reichert <yonatanreichert@gmail.com>
2026-08-02 10:43:56 +0300

    fixed login service

--- backend/src/http/routes/auth/controllers/loginController.ts
+++ backend/src/http/routes/auth/controllers/loginController.ts
@@ -1,9 +1,5 @@
 import { Context } from "koa";
-import { UserRepository } from "@/db/repositories/UserRepository";
-import { NotFoundError } from "@/http/shared/error/http_error";
 import { HTTP_STATUS } from "@/http/shared/status/httpStatus";
-import bcrypt from "bcrypt";
-import { BoardStateRepository } from "@/db/repositories/BoardStateRepository";
 import { loginService } from "@/services/auth/loginService";
 
 export async function loginController(ctx: Context) {
@@ -12,21 +8,13 @@ export async function loginController(ctx: Context) {
         password,
         board_id: board_template_id,
     } = ctx.state.validated.body;
-    const board = await loginService(email, password, board_template_id);
-    const user = await UserRepository.getUser(email);
-
-    if (user === null) {
-        throw new NotFoundError("User doesn't exist");
-    }
-    if (!(await bcrypt.compare(password, user.hashed_password))) {
-        throw new NotFoundError("Incorrect password");
-    }
-    const board = await BoardStateRepository.getLatestBoardState(
+    const { board, user_id } = await loginService(
+        email,
+        password,
         board_template_id,
-        user.user_id,
     );
 
-    ctx.session.user_id = user.user_id;
+    ctx.session.user_id = user_id;
     ctx.session.board_id = board_template_id;
     ctx.status = HTTP_STATUS.OK;
     ctx.body = { board };
--- backend/src/services/auth/loginService.ts
+++ backend/src/services/auth/loginService.ts
@@ -1,5 +1,25 @@
+import bcrypt from "bcrypt";
+import { BoardStateRepository } from "@/db/repositories/BoardStateRepository";
+import { UserRepository } from "@/db/repositories/UserRepository";
+import { NotFoundError } from "@/http/shared/error/http_error";
+import { Board } from "@/shared/types";
+
 export async function loginService(
     email: string,
     password: string,
     board_template_id: string,
-) {}
+): Promise<{ board: Board; user_id: string }> {
+    const user = await UserRepository.getUser(email);
+
+    if (user === null) {
+        throw new NotFoundError("User doesn't exist");
+    }
+    if (!(await bcrypt.compare(password, user.hashed_password))) {
+        throw new NotFoundError("Incorrect password");
+    }
+    const board = await BoardStateRepository.getLatestBoardState(
+        board_template_id,
+        user.user_id,
+    );
+    return { board, user_id: user.user_id };
+}
